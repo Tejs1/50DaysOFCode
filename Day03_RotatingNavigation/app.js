76b320b54636d5f23d3c6a2d95724585fff9d9d8
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-27 06:14:06 +0530

    moved event listener from button to circle

--- Day03_RotatingNavigation/app.js
+++ Day03_RotatingNavigation/app.js
@@ -1,6 +1,4 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
 const container = document.querySelector(".container");
+const circle = document.querySelector(".circle");
 
-open.addEventListener("click", () => container.classList.add("show-nav"));
-close.addEventListener("click", () => container.classList.remove("show-nav"));
+circle.addEventListener("click", () => container.classList.toggle("show-nav"));
