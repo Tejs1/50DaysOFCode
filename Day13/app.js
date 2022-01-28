9e6e327dfe81dda072fdb42e3aec06d19ea1fdc2
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2022-01-28 11:53:14 +0530

    component usablity

---
+++ Day13/app.js
@@ -0,0 +1,6 @@
+const img = document.querySelector(".iii");
+img.setAttribute(
+  "src",
+  "https://upbeat-easley-ce8c96.netlify.app/components/avatar/images/fallback.svg"
+);
+// Avatar/Components/avatar/images/avatar-4.png
